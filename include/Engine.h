26a306512d5be6c5ffa1eb189cddd58a7e98512b
Skygad <schweizer.gaddiel@gmail.com>
2023-04-13 22:56:23 +0200

    Create Engine Files

---
+++ include/Engine.h
@@ -0,0 +1,26 @@
+#include <common.h>
+#ifdef ENGINE
+
+#ifndef ENGINE_FILE_H
+#define ENGINE_FILE_H
+
+#include <Arduino.h>
+
+/// @brief Setup function of module
+void MySetup();
+
+/// @brief Reset function of module
+void ResetModule();
+
+/// @brief Loop function of module
+void MyLoop();
+
+/// @brief Function called when a message is received
+void Received();
+
+/// @brief Function called when a message is send in close loop
+void ServerSimulation();
+
+
+#endif // FILE_H
+#endif // ENGINE
