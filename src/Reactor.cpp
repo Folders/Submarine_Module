d0ae34dd7e3f8eae827393f3acedbf887001f46f
Pierre <clot.p@the-machines.ch>
2023-05-05 00:08:32 +0200

    Testing on ESP32

--- src/Reactor.cpp
+++ src/Reactor.cpp
@@ -26,34 +26,34 @@ Ticker Time_Update;
 #ifdef ESP8266
 
 // TFT for ESP3266
-#define TFT_RST -1  // RST
-#define TFT_DC 15   // D8
-#define TFT_MOSI 13 // D7
+#define TFT_RST -1 // RST
+#define TFT_DC 15  // D8
+
+#define TFT_SCK 14  // D5
 #define TFT_MISO 12 // D6
-#define TFT_CLK 14  // D5
+#define TFT_MOSI 13 // D7
 #define TFT_CS 16   // D0
 
 #elif defined(ESP32)
+
 // TFT for ESP32
-#define TFT_RST 33  // RST
-#define TFT_DC 27   // D8
-#define TFT_MOSI 15 // D7
-#define TFT_MISO 16 // D6
-#define TFT_CLK 17  // D5
-#define TFT_CS 14   // D0
+#define TFT_RST -1 // RST
+#define TFT_DC 12  // D8
+
+#define TFT_SCK 7   // Ok
+#define TFT_MISO 9  // Ok
+#define TFT_MOSI 11 // Ok
+#define TFT_CS 5    // D0
+
 #endif
 
-Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO); // screen
+// TFT screen
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCK, TFT_RST, TFT_MISO);
 
 /////////////////////////////////////////////////////////////////////////////////////////////
 //                                      User function                                      //
 /////////////////////////////////////////////////////////////////////////////////////////////
-
-/// @brief Si je tape ///, il me propose de mettre des commentaires à la fonction
-void my_function()
-{
-    // ...
-}
+#pragma region "User function"
 
 /// @brief Draw a standard gauge componant
 /// @param width Width of the gauge [pix]
@@ -66,7 +66,6 @@ void my_function()
 /// @param color_high Color of the high side of gauge
 void DrawGauge(int16_t width, int16_t height, int16_t left, int16_t top, float filling, float tol, uint16_t color_low, uint16_t color_high)
 {
-    // DrawGauge(21, 181, 50, 0, 290, 10, ILI9341_BLUE, ILI9341_RED);
 
     bool IsVert;
     int16_t radius;
@@ -91,12 +90,13 @@ void DrawGauge(int16_t width, int16_t height, int16_t left, int16_t top, float f
         length = width - radius - radius;
     }
 
-    // Gauge computation
+    /* Gauge computation
     //                     pos_gauge (filling)
-    //                     /
-    //         (       |  |  |              )
-    //         /      /       \             \
-    //        0    pos_low    pos_high      length    ->  [pixel]
+    //                      /
+    //          (       |  |  |              )
+    //          /      /       \             \
+    //         0    pos_low    pos_high      length    ->  [pixel]
+    */
 
     // Compute filling gauge position
     float pas = static_cast<float>(length) / 100.0;
@@ -120,7 +120,6 @@ void DrawGauge(int16_t width, int16_t height, int16_t left, int16_t top, float f
     // Outside rectangle
     canvas.fillRoundRect(0, 0, width, height, radius, ILI9341_WHITE);
 
-
     // Draw circle of higher value. If the value is at maximum, don't draw the circle
     if (pos_high < length)
     {
@@ -136,7 +135,6 @@ void DrawGauge(int16_t width, int16_t height, int16_t left, int16_t top, float f
         }
     }
 
-
     // Draw circle of lower value. If the value is at minumum, don't draw the circle
     if (pos_low > 0)
     {
@@ -151,7 +149,6 @@ void DrawGauge(int16_t width, int16_t height, int16_t left, int16_t top, float f
             canvas.fillCircle(radius, radius, radius - 2, color_low);
         }
     }
-
 
     // Write near rectangle (Two pixel smaller). On vertical, the near = the high value
     if (IsVert)
@@ -175,17 +172,13 @@ void DrawGauge(int16_t width, int16_t height, int16_t left, int16_t top, float f
         canvas.fillRect(radius + pix_low, 2, pix_high - pix_low, height - 4, ILI9341_WHITE);
     }
 
-
     // Put the canvas at the right side
     tft.drawRGBBitmap(left, top, canvas.getBuffer(), canvas.width(), canvas.height());
 }
 
-
-/// @brief Draw the power gauge. Size: 21x181 - Pos: 290;10 
+/// @brief Draw the power gauge. Size: 21x181 - Pos: 290;10
 void Draw_PowerGauge()
 {
-    // Size max: 320x240
-
     // Draw power gauge
     DrawGauge(21, 181, 290, 10, _ConsoPower, _Tol, ILI9341_MAROON, ILI9341_YELLOW);
 }
@@ -193,18 +186,16 @@ void Draw_PowerGauge()
 /// @brief Draw the temperature gauge. Size: 250x21 - Pos: 10;210
 void Draw_TempGauge()
 {
-    // Size 320x240
-
     // Test horizontal power gauge
     DrawGauge(250, 21, 10, 210, _TempNeeded, _Tol, ILI9341_BLUE, ILI9341_RED);
 }
 
-int mapInt(int x, int in_min, int in_max, int out_min, int out_max) {
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+int mapInt(int x, int in_min, int in_max, int out_min, int out_max)
+{
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-
-void drawLine(GFXcanvas1& canvas, int x0, int y0, int x1, int y1, uint16_t color) 
+void drawLine(GFXcanvas1 &canvas, int x0, int y0, int x1, int y1, uint16_t color)
 {
 
     // Calculer les différences entre les coordonnées des deux points
@@ -220,37 +211,39 @@ void drawLine(GFXcanvas1& canvas, int x0, int y0, int x1, int y1, uint16_t color
     int e2;
 
     // Boucle principale de dessin
-    while (true) {
+    while (true)
+    {
         // Dessiner le pixel courant
         canvas.drawPixel(x0, y0, color);
 
         // Vérifier si on est arrivé au point final
-        if (x0 == x1 && y0 == y1) {
-        break;
+        if (x0 == x1 && y0 == y1)
+        {
+            break;
         }
 
         // Calculer l'erreur pour le prochain pixel
         e2 = 2 * err;
 
         // Corriger l'erreur si nécessaire et avancer d'un pixel
-        if (e2 > -dy) {
-        err -= dy;
-        x0 += sx;
+        if (e2 > -dy)
+        {
+            err -= dy;
+            x0 += sx;
         }
-        if (e2 < dx) {
-        err += dx;
-        y0 += sy;
+        if (e2 < dx)
+        {
+            err += dx;
+            y0 += sy;
         }
     }
 }
 
-
-
-#define GRAPH_WIDTH 250     // Multiple of graph step
-#define GRAPH_HEIGHT 200    //
-#define GRAPH_TOP 2         // Min = 2
-#define GRAPH_LEFT 2        // Min = 2
-#define GRAPH_STEP 5       
+#define GRAPH_WIDTH 250  // Multiple of graph step
+#define GRAPH_HEIGHT 200 //
+#define GRAPH_TOP 2      // Min = 2
+#define GRAPH_LEFT 2     // Min = 2
+#define GRAPH_STEP 5
 
 int _graphInd;
 int _graphMax;
@@ -259,19 +252,19 @@ int _graphOld = 0;
 void Draw_Graph()
 {
     // Canvas of power gauge
-    GFXcanvas1  canvas(GRAPH_STEP, GRAPH_HEIGHT);
- 
+    GFXcanvas1 canvas(GRAPH_STEP, GRAPH_HEIGHT);
+
     // Get a random value in range [%]
-    int val = (rand() % (_Tol*2+1) );
+    int val = (rand() % (_Tol * 2 + 1));
     val = _ConsoPower + val - _Tol;
 
 #ifdef LOG
     Serial.printf("Power is %i%% and the tolerance is ±%i%%. The random value is %i%%\n", _ConsoPower, _Tol, val);
 #endif
-    
+
     // Define trace point
-    int y0  = mapInt(_graphOld, 0, 100, GRAPH_HEIGHT, 0);
-    int y1    = mapInt(val, 0, 100, GRAPH_HEIGHT, 0);
+    int y0 = mapInt(_graphOld, 0, 100, GRAPH_HEIGHT, 0);
+    int y1 = mapInt(val, 0, 100, GRAPH_HEIGHT, 0);
     int x0 = 0;
     int x1 = GRAPH_STEP - 1;
 
@@ -279,33 +272,32 @@ void Draw_Graph()
     if (_graphInd < GRAPH_WIDTH - GRAPH_STEP)
     {
         // Draw triangle to the end position
-        drawLine(canvas, 0, y1, 3,y1+3, 0xFFFF);
-        drawLine(canvas, 0, y1, 3,y1-3, 0xFFFF);
-        drawLine(canvas, 3, y1-3, 3,y1+3, 0xFFFF);
+        drawLine(canvas, 0, y1, 3, y1 + 3, 0xFFFF);
+        drawLine(canvas, 0, y1, 3, y1 - 3, 0xFFFF);
+        drawLine(canvas, 3, y1 - 3, 3, y1 + 3, 0xFFFF);
 
         // Put in next emplacement the triangle
         tft.drawBitmap(GRAPH_LEFT + _graphInd + GRAPH_STEP, GRAPH_TOP, canvas.getBuffer(), canvas.width(), canvas.height(), ILI9341_RED, 0x0000);
-        
-        // Undraw triangle to the end position
-        drawLine(canvas, 0, y1, 3,y1+3, 0x0000);
-        drawLine(canvas, 0, y1, 3,y1-3, 0x0000);
-        drawLine(canvas, 3, y1-3, 3,y1+3, 0x0000);
-    }
 
+        // Undraw triangle to the end position
+        drawLine(canvas, 0, y1, 3, y1 + 3, 0x0000);
+        drawLine(canvas, 0, y1, 3, y1 - 3, 0x0000);
+        drawLine(canvas, 3, y1 - 3, 3, y1 + 3, 0x0000);
+    }
 
 #ifdef LOG
     Serial.printf("Line go from (%i;%i) to (%i;%i)\n", x0, y0, x1, y1);
 #endif
 
     ////////////////////// Normal line  -> STACK OVERFLOW
-    //tft.drawLine(0, startY, GRAPH_STEP-1, endY, 0xFFFF);
-    
+    // tft.drawLine(0, startY, GRAPH_STEP-1, endY, 0xFFFF);
+
     // Draw the line to the new random position
     drawLine(canvas, x0, y0, x1, y1, 0xFFFF);
 
     // Draw de canvas
     tft.drawBitmap(GRAPH_LEFT + _graphInd, GRAPH_TOP, canvas.getBuffer(), canvas.width(), canvas.height(), 0xFFFF, 0x0000);
-    
+
     // Update graph position
     _graphOld = val;
     _graphInd += GRAPH_STEP;
@@ -320,6 +312,9 @@ void Draw_Update()
     Draw_Graph();
 }
 
+#pragma endregion
+
+
 /////////////////////////////////////////////////////////////////////////////////////////////
 //                                     Setup and reset                                     //
 /////////////////////////////////////////////////////////////////////////////////////////////
@@ -330,8 +325,6 @@ void MySetup()
     // Start TFT
     tft.begin();        // start the screen
     tft.setRotation(1); // landscreen, connexion up
-
-
 }
 
 ///////////////////////////////  Reset all proprety of module  ////////////////////////////////
@@ -349,15 +342,15 @@ void ResetModule()
     Draw_TempGauge();
 
     // Draw external rectangle of graphique
-    tft.drawRect(GRAPH_LEFT-1, GRAPH_TOP-1, GRAPH_WIDTH+2, GRAPH_HEIGHT+2, ILI9341_WHITE);
-    tft.drawRect(GRAPH_LEFT-2, GRAPH_TOP-2, GRAPH_WIDTH+4, GRAPH_HEIGHT+4, ILI9341_WHITE);
-    
+    tft.drawRect(GRAPH_LEFT - 1, GRAPH_TOP - 1, GRAPH_WIDTH + 2, GRAPH_HEIGHT + 2, ILI9341_WHITE);
+    tft.drawRect(GRAPH_LEFT - 2, GRAPH_TOP - 2, GRAPH_WIDTH + 4, GRAPH_HEIGHT + 4, ILI9341_WHITE);
+
     // And the graphique
-    //Draw_Graph();
+    // Draw_Graph();
 
 #ifdef STANDALONE
     // Trigger timer every second in standalone
-    if( !Time_Update.active() )
+    if (!Time_Update.active())
         Time_Update.attach(1, Draw_Update);
 #endif
 }
@@ -372,6 +365,7 @@ void MyLoop()
 /////////////////////////////////////////////////////////////////////////////////////////////
 //                                      Communication                                      //
 /////////////////////////////////////////////////////////////////////////////////////////////
+#pragma region "Communication"
 
 /// @brief Call when a message is received from server (or serial)
 void Received()
@@ -407,7 +401,6 @@ void Received()
             Draw_PowerGauge();
         }
     }
-
 
     // The get the received code, use the function GetCode()
     if (comm.GetCode() == "SPT")
@@ -459,5 +452,6 @@ void ServerSimulation()
         Serial.println("WOOW, j'ai reçu un NVC en local !");
     }
 }
+#pragma endregion
 
 #endif
