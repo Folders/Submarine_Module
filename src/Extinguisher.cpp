af823d201062d6191a0b3b82c1ca832cafaf696d
danansermin <danansermin@gmail.com>
2024-02-29 11:24:56 -0500

    Clean code

--- src/Extinguisher.cpp
+++ src/Extinguisher.cpp
@@ -6,7 +6,7 @@
 //////// Add new include library
 #include <Wire.h>
 #include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>       //charge the Adafruit_SSD1306 Wemos Mini OLED !
+#include <Adafruit_SSD1306.h> //charge the Adafruit_SSD1306 Wemos Mini OLED !
 #include <Bounce2.h>
 #include <Ticker.h>
 #include "SoundData.h"
@@ -15,12 +15,12 @@
 ////////  Define global constantes      (ALWAYS IN MAJ)
 
 // OLED
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_WIDTH 128    // OLED display width, in pixels
+#define SCREEN_HEIGHT 64    // OLED display height, in pixels
+#define OLED_RESET -1       // Reset pin # (or -1 if sharing Arduino reset pin)
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire,OLED_RESET);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 // IR
 const int LED_IR = 4;
@@ -30,10 +30,9 @@ const int LED_IR = 4;
 #define CONTACT 19
 
 // Song
-//#define PLAY_PIN 15
+// #define PLAY_PIN 15
 
-const float ANIM_FREQUENCE = 0.6;
-const int INTERRUPT_DELAY = 60;
+const float ANIM_FREQUENCE = 0.6; // animation frequencie
 
 ////////  Define global variables
 
@@ -47,8 +46,6 @@ int percent;
 byte batt_lvl_charging = 9;
 byte batt_lvl_refill;
 
-//bool filling_state;
-
 bool arrowcolor;
 bool lightingcolor;
 bool a_arrowcolor;
@@ -56,16 +53,13 @@ bool a_lightingcolor;
 bool trigger_button;
 bool contact_button;
 
-
-XT_Wav_Class ForceWithYou(Force);     // create an object of type XT_Wav_Class that is used by 
-                                      // the dac audio class (below), passing wav data as parameter.
-                                      
-XT_DAC_Audio_Class DacAudio(25,0);    // Create the main player class object. 
-                                      // Use GPIO 25, one of the 2 DAC pins and timer 0
-
-uint32_t DemoCounter=0;               // Just a counter to use in the serial monitor
-                                      // not essential to playing the sound
-
+// Sounds settings
+XT_Wav_Class ForceWithYou(Force);   // create an object of type XT_Wav_Class that is used by
+                                    // the dac audio class (below), passing wav data as parameter.
+XT_DAC_Audio_Class DacAudio(25, 0); // Create the main player class object.
+                                    // Use GPIO 25, one of the 2 DAC pins and timer 0
+uint32_t DemoCounter = 0;           // Just a counter to use in the serial monitor
+                                    // not essential to playing the sound
 
 /// @brief make cligning the arrow
 void clign_symbol_arrow()
@@ -101,11 +95,11 @@ void using_battery_animation() // for using battery animation
     }
 }
 
+// Tickers
 Ticker _Clign(clign_symbol_arrow, 1000, 0, MILLIS);
 Ticker _Clign_1(clign_symbol_lighting, 1000, 0, MILLIS);
-Ticker _Animation(refill_battery_animation, (1000*ANIM_FREQUENCE), 0, MILLIS);
-Ticker _Animation_1(using_battery_animation, (1000*ANIM_FREQUENCE), 0, MILLIS);
-
+Ticker _Animation(refill_battery_animation, (1000 * ANIM_FREQUENCE), 0, MILLIS);
+Ticker _Animation_1(using_battery_animation, (1000 * ANIM_FREQUENCE), 0, MILLIS);
 
 /////////////////////////////////////////////////////////////////////////////////////////////
 //                                      User function                                      //
@@ -114,36 +108,40 @@ Ticker _Animation_1(using_battery_animation, (1000*ANIM_FREQUENCE), 0, MILLIS);
 /// @brief //display percent on OLED
 void display_percent()
 {
-    display.display();
     display.clearDisplay();
     display.setTextColor(WHITE);
 
-    // Ajustez la taille et la position en fonction de la résolution plus grande
-    if (percent < 10 && percent > 0) { // for centering the text when 1-10
-        display.setTextSize(5); // Ajusté pour l'écran plus grand
-        display.setCursor(40, 15); // Centré sur un écran plus large
+    if (percent < 10 && percent > 0)
+    { // for centering the text when 1-10
+        display.setTextSize(5);
+        display.setCursor(40, 15);
         display.print(percent);
         display.print("%");
-    } else if (percent == 0) { //bigger size if percent = 0
+    }
+    else if (percent == 0)
+    { // bigger size if percent = 0
         display.setTextSize(2);
-        display.setCursor(55, 15); // Ajustement pour le centrage
+        display.setCursor(55, 15);
         display.print("0%");
         display.println();
-        display.setCursor(38, 35); 
+        display.setCursor(38, 35);
         display.println("vide!");
-    } else if (percent == 100) { //smaller size if percent = 100
+    }
+    else if (percent == 100)
+    { // smaller size if percent = 100
         display.setTextSize(4);
-        display.setCursor(18, 18); // Ajusté pour l'écran plus grand
+        display.setCursor(18, 18);
         display.print("100%");
-    } else if (percent >= 10 && percent < 100) { // affich the percent with the "%" symbol
+    }
+    else if (percent >= 10 && percent < 100)
+    {
         display.setTextSize(4);
-        display.setCursor(32, 17); // Ajusté pour un écran plus large
+        display.setCursor(32, 17);
         display.print(percent);
         display.print("%");
     }
 
     display.display();
-    
 }
 
 /// @brief draw a battery symbol
@@ -152,52 +150,56 @@ void display_battery_unfill()
     display.clearDisplay();
 
     // Draw the outer battery rectangle
-    display.drawRect(20, 10, 90, 44, WHITE); // Outer rectangle adjusted to specified dimensions
+    display.drawRect(20, 10, 90, 44, WHITE);
 
     // Draw the "head" of the battery on the right side
-    display.drawRect(110, 24, 4, 16, WHITE); // Position and dimensions adjusted for "head"
+    display.drawRect(110, 24, 4, 16, WHITE);
 
     // Draw the battery level indicators inside
-    for (int i = 0; i < 11; i++) {
-        // Adjust the positions and dimensions to fit within the new battery outline
-        display.fillRect((22 + i * 8), 12, 6, 40, WHITE); // Spacing adjusted for inside the rectangle
+    for (int i = 0; i < 11; i++)
+    {
+        display.fillRect((22 + i * 8), 12, 6, 40, WHITE);
     }
 
-  
-    a_arrowcolor = arrowcolor ? BLACK : WHITE; // change arrow's logo color
-    display.fillRect(8, 29, 8, 5, a_arrowcolor); // Ajusté pour l'écran plus grand
-    display.fillTriangle(2, 31, 8, 27, 8, 35, a_arrowcolor); // 
-    
-    for (int i = 0; i < 11; i++) {
-    if (i < batt_lvl_charging) {
-        // Effacez chaque rectangle en fonction du niveau de déchargement
-        // En noircissant les rectangles de droite à gauche
-        display.fillRect((22 + (10 - i) * 8), 12, 6, 40, BLACK);
+    // unfill effect
+    for (int i = 0; i < 11; i++)
+    {
+        if (i < batt_lvl_charging)
+        {
+            display.fillRect((22 + (10 - i) * 8), 12, 6, 40, BLACK);
+        }
     }
-}
+
+    // Arrow symbol
+    a_arrowcolor = arrowcolor ? BLACK : WHITE; // change arrow's logo color
+    display.fillRect(8, 29, 8, 5, a_arrowcolor);
+    display.fillTriangle(2, 31, 8, 27, 8, 35, a_arrowcolor); //
     display.display();
 }
 
 void display_battery_refill()
 {
-   display.clearDisplay();
+    display.clearDisplay();
     // Draw the outer battery rectangle
-    display.drawRect(20, 10, 90, 44, WHITE); // Outer rectangle adjusted to specified dimensions
+    display.drawRect(20, 10, 90, 44, WHITE);
 
     // Draw the "head" of the battery on the right side
-    display.drawRect(110, 24, 4, 16, WHITE); // Position and dimensions adjusted for "head"
+    display.drawRect(110, 24, 4, 16, WHITE);
 
-      for (int i = 0; i <= batt_lvl_refill && i <= 11; i++) {
-        if (i<=10){
-        display.fillRect((22 + i * 8), 12, 6, 40, WHITE); // Remplit les rectangles un par un
+    // Refill effect
+    for (int i = 0; i <= batt_lvl_refill && i <= 11; i++)
+    {
+        if (i <= 10)
+        {
+            display.fillRect((22 + i * 8), 12, 6, 40, WHITE);
         }
-        if (i==11)
-      {
-        display.fillRect(21, 11, 88, 42, BLACK);
-      }
+        if (i == 11)
+        {
+            display.fillRect(21, 11, 88, 42, BLACK); // fill the battery in black
+        }
     }
 
-        // lighting symbol
+    // lighting symbol
     a_lightingcolor = lightingcolor ? BLACK : WHITE; // change lighing's symbol color
     display.fillTriangle(12, 26, 8, 33, 12, 31, a_lightingcolor);
     display.fillTriangle(10, 37, 10, 32, 14, 29, a_lightingcolor);
@@ -205,61 +207,12 @@ void display_battery_refill()
     display.display();
 }
 
-/*
-/// @brief refilling animation
-void refilling()
-{
-    // lighting symbol
-    a_lightingcolor = lightingcolor ? BLACK : WHITE; // change lighing's symbol color
-    display.fillTriangle(32, 36, 28, 43, 32, 41, a_lightingcolor);
-    display.fillTriangle(30, 47, 30, 42, 34, 39, a_lightingcolor);
-
-    // refilling effect
-    for (int i = 0; i < 10; i++)
-    {
-        display.drawRect((45 - (i * 3)), 16, 2, 16, WHITE);
-    }
-    display.fillRect((17 + (batt_lvl_refill * 3)), 17, (29 - (batt_lvl_refill * 3)), 14, BLACK);
-
-    display.display();
-}
-
-/// @brief unfilling animation
-void unfilling()
-
-{
-    // ARROW symbol
-    a_arrowcolor = arrowcolor ? BLACK : WHITE; // change arrow's logo color
-    display.fillRect(28, 40, 10, 2, a_arrowcolor);
-    display.fillTriangle(23, 41, 28, 38, 28, 44, a_arrowcolor);
-
-    // unfilling effect
-    display.fillRect((44 - (batt_lvl_charging * 3)), 17, (2 + (batt_lvl_charging * 3)), 14, BLACK);
-
-    if (batt_lvl_charging > 8)
-    {
-        for (int i = 0; i < 10; i++)
-        {
-            display.drawRect((45 - (i * 3)), 16, 2, 16, WHITE);
-        }
-    }
-    display.display();
-}
-*/
-
-
-/// @brief play the song with a delay when using
+/// @brief play the song
 void play()
 {
-    DacAudio.FillBuffer();                // Fill the sound buffer with data
-  if(ForceWithYou.Playing==false)         // if not playing,
-    DacAudio.Play(&ForceWithYou);         // play it, this will cause it to repeat and repeat...
-    /*
-    // turn of first for restart the song with a small delay
-    digitalWrite(PLAY_PIN, LOW);
-    _tempo.once_ms(INTERRUPT_DELAY, []()
-                   { digitalWrite(PLAY_PIN, HIGH); });
-    */
+    DacAudio.FillBuffer();             // Fill the sound buffer with data
+    if (ForceWithYou.Playing == false) // if not playing,
+        DacAudio.Play(&ForceWithYou);  // play it, this will cause it to repeat and repeat...
 }
 
 /// @brief read buttons
@@ -270,10 +223,10 @@ void read_buttons()
 
     if (trigger.fell()) // trigger's button has been pressed
     {
-        comm.send("EXR;1");     //send trigger's on to server
-        #ifdef LOG
-    Serial.println("Trigger button pressed");
-#endif  
+        comm.send("EXR;1"); // send trigger's on to server
+#ifdef LOG
+        Serial.println("Trigger button pressed");
+#endif
 
         if (percent > 0)
         {
@@ -282,18 +235,8 @@ void read_buttons()
             trigger_button = true;
             batt_lvl_charging = 9;
 
-            // start animation
-          //  display.clearDisplay();
-           //display_battery_unfill();
-           // _Animation.attach(ANIM_FREQUENCE, battery_animation);
-           // _Clign.attach(1, clign_symbol);
-
             // play the song
             play();
-            /*
-            digitalWrite(PLAY_PIN, HIGH);
-            _play.attach(8, play);
-            */
         }
         else // if percent = 0 do nothing
         {
@@ -303,48 +246,30 @@ void read_buttons()
 
     else if (trigger.rose()) // trigger's button has been released
     {
-        comm.send("EXR;0");     //send trigger's off to server
-                #ifdef LOG
-    Serial.println("Trigger button released");
-#endif  
+        comm.send("EXR;0"); // send trigger's off to server
+#ifdef LOG
+        Serial.println("Trigger button released");
+#endif
 
         // turn off the song
+        DacAudio.StopAllSounds();
 
-         DacAudio.StopAllSounds() ;
-
-        /*
-        digitalWrite(PLAY_PIN, LOW);
-        _play.detach();
-        */
-       
         // turn off the IR led
         digitalWrite(LED_IR, LOW);
 
         trigger_button = false;
-
-       // display.clearDisplay();
-
-        // stop animation
-       // _Animation.detach();
-       // _Clign.detach();
     }
 
     if (contact.fell()) // contact's button has been pressed
     {
-        comm.send("EXC;1");     //send contact's on to server
-                #ifdef LOG
-    Serial.println("Contact button pressed");
-#endif  
+        comm.send("EXC;1"); // send contact's on to server
+#ifdef LOG
+        Serial.println("Contact button pressed");
+#endif
 
         if (percent < 100)
         {
             contact_button = true;
-         //   display.clearDisplay();
-
-            // start charging animation
-        //    display_battery_refill();
-           // _Animation_1.attach(ANIM_FREQUENCE, battery_animation_1);
-           // _Clign_1.attach(1, clign_symbol_1);
         }
 
         else // if percent = 100, do nothing and stop animation
@@ -355,17 +280,12 @@ void read_buttons()
 
     else if (contact.rose()) // contact's button has been released
     {
-        comm.send("EXC;0");     //send contact's off to server
-                #ifdef LOG
-    Serial.println("Contact button released");
-#endif  
+        comm.send("EXC;0"); // send contact's off to server
+#ifdef LOG
+        Serial.println("Contact button released");
+#endif
 
         contact_button = false;
-
-        // stop charging's animation
-       // display.clearDisplay();
-       // _Animation_1.detach();
-        // _Clign_1.detach();
     }
 }
 
@@ -388,37 +308,29 @@ void MySetup()
     pinMode(LED_IR, OUTPUT);
     digitalWrite(LED_IR, LOW);
 
-    // SONG output
-    /*
-    pinMode(PLAY_PIN, OUTPUT);
-    digitalWrite(PLAY_PIN, LOW);
-    */
-
     percent = 100;
 
     // display setup
-if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
-  }
+    if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
+    {
+        Serial.println(F("SSD1306 allocation failed"));
+        for (;;)
+            ; // Don't proceed, loop forever
+    }
 
-  display.display();
-  // Clear the buffer
-  display.clearDisplay();
-  display.display();
+    // Clear the buffer
+    display.clearDisplay();
+    display.display();
 
 #ifdef LOG
     Serial.println("Start");
-#endif  
+#endif
 
-_Clign.start();
-_Clign_1.start();
-_Animation.start();
-_Animation_1.start();
-
-    // init temporisator for the song Ticker
-    //_tempo.attach_ms(1, []() {});
-   // _tempo.detach();
+    // Start tickers
+    _Clign.start();
+    _Clign_1.start();
+    _Animation.start();
+    _Animation_1.start();
 }
 
 ///////////////////////////////  Reset all proprety of module  ////////////////////////////////
@@ -433,25 +345,12 @@ void ResetModule()
 /// @brief Call at the end of the main loop function
 void MyLoop()
 {
-    /*
-    // To send datas to the server, use the send function
-    comm.send("LED;R;1");
-
-    // It's possible to send with more then on line
-    comm.start();      // Open the buffer
-    comm.add("LED");   // Write String
-    comm.add(';');     // Add char
-    //comm.add(testInt); // Add from variable
-    comm.send();       // Send concatened variable
-    */
-
+    // Update tickers
     _Clign.update();
     _Clign_1.update();
     _Animation.update();
-     _Animation_1.update();
+    _Animation_1.update();
 
-
-    
     read_buttons(); // read buttons
 
     if (trigger_button == false && contact_button == false) // no button pressed
@@ -468,8 +367,6 @@ void MyLoop()
     {
         display_battery_refill(); // refilling animation
     }
-
-    
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////
@@ -511,11 +408,10 @@ void ServerSimulation()
         Serial.println("WOOW, j'ai reçu un NVC en local !");
     }
 
-
     // decrease percent if trigger's on
     if (comm.GetCode() == "EXR" && comm.GetParameter(1) == "1")
     {
-        
+
         // DO something
         if (percent > 5)
         {
@@ -524,11 +420,10 @@ void ServerSimulation()
 
         else
         {
-             percent = 0;
+            percent = 0;
         }
-           
     }
-     
+
     // decrease percent if trigger's on
     if (comm.GetCode() == "EXC" && comm.GetParameter(1) == "1")
     {
@@ -537,13 +432,11 @@ void ServerSimulation()
         {
             percent = percent + 5;
         }
-        else 
+        else
         {
             percent = 100;
         }
-            
     }
-
 }
 
 #endif
